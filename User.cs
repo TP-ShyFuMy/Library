da13b891649f3300fe1e3f97f1829a2f9625a9f2
Miguel-Gremy <miguel.gremy72@gmail.com>
2020-10-05 12:02:57 +0200

    Add package SockNet

---
+++ User.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Net;
+using HashidsNet;
+
+namespace TPShyFuMyLibrary
+{
+    public class User
+    {
+        public IPAddress IpAddress { get; set; }
+        public String userName { get; set; }
+        public String userId { get; set; }
+    }
+}
